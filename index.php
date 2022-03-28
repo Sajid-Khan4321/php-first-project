bae3f17a4b5af412acc47dfe3c26bf07ca47a933
Sajid <rwaji953@gmail.com>
2022-03-28 15:40:38 -0600

    first commt

---
+++ index.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE <!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Page Title - My Site</title> <!-- TODO: update page title -->
+    <script type="module">
+            document.documentElement.classList.remove('no-js');
+            document.documentElement.classList.add('js');
+    </script>
+    <meta name="description" content="Page Description">
+    <meta property="og:title" content="Home - Page">  
+    <meta property="og:url" content="https://www.example.com/page"> 
+    <meta property="og:description" content="OG Page Description">  
+    <meta property="og:image" content="/path/to/image">  
+    <meta property="og:image:alt" content="OG Image Description">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta name="twitter:card" content="large-image-twitter.jpg"> <!-- TODO: update twitter:cared -->
+    <link rel="canonical" href="https://www.example.com/page"> <!-- TODO: update canonical link -->
+    <link rel="icon" href="/favicon.ico">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="manifest" href="site.webmanifest"> <!-- TODO: update web app manifest file -->
+    <meta name="theme-color" content="#FF00FF"> <!-- TODO: update meta theme color -->
+    <link rel="stylesheet" href="/styles/css/styles.css">  <!-- TODO: Update styles -->
+    <link rel="stylesheet" href="/styles/css/print.css" media="print">
+    <div class="mb-3 row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword">
+    </div>
+  </div>
+</head>
+<body>
+   
+    <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+</html>>
